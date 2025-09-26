88d7cd6544de109af01ccb7a73b9ff50995f01ee
Mark de Nijs <100344788+MurkDeNoes@users.noreply.github.com>
2025-09-26 09:53:25 +0200

    Cleanup files

--- learning-the-basics/app.js
+++ learning-the-basics/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const requestHandler = require('./routes')
+const requestHandler = require('./routes');
 
 const server = http.createServer(requestHandler);
 
--- learning-the-basics/routes.js
+++ learning-the-basics/routes.js
@@ -16,9 +16,9 @@ const requestHandler = (req, res) => {
 
     if (url === '/message' && method === 'POST') {
         const body = [];
+
         req.on('data', chunk => {
-            console.log(chunk)
-            body.push(chunk)
+            body.push(chunk);
         });
 
         return req.on('end', () => {
