76417de2d8e92f3a20fbb4732edf0e6b8badcd97
Mark de Nijs <100344788+MurkDeNoes@users.noreply.github.com>
2025-09-26 17:59:13 +0200

    Add a post middleware for the /product route and use the body-parser module

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<h1>The "Add Product" Page</h1>');
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/add-product', (req, res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product </button></form>');
 })
 
-app.use('/', (req, res, next) => {
+app.post('/product', (req, res) => {
+    console.log(req.body.title)
+    res.redirect('/');
+})
+
+app.use('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 })
 
