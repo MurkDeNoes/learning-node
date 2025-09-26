ba4eee3968c65a26459f7fce66aa4b386fc034aa
Mark de Nijs <100344788+MurkDeNoes@users.noreply.github.com>
2025-09-26 20:17:48 +0200

    Remove the catch all use route from app.js

--- expressjs/app.js
+++ expressjs/app.js
@@ -11,8 +11,4 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-})
-
 app.listen(3000);
