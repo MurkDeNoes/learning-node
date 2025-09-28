a2a0c5c396b5127b1b2a4219d340fd434580bd06
Mark de Nijs <100344788+MurkDeNoes@users.noreply.github.com>
2025-09-28 11:59:43 +0200

    Add a dynamic route and make use of express params

--- dynamic-routes/models/product.js
+++ dynamic-routes/models/product.js
@@ -7,34 +7,36 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
+const getProductsFromFile = callback => {
+    fs.readFile(p, (error, fileContent) => {
+        if (error) {
+            callback([]);
+        } else {
+            callback(JSON.parse(fileContent));
+        }
+    });
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+        this.id = Math.random() * 100;
+    }
 
-  save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+            fs.writeFile(p, JSON.stringify(products), error => {
+                console.log(error);
+            });
+        });
+    }
+
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
 };
